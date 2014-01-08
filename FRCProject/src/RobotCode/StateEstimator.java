56098608d03706974f0cdfe902063d08e2123ae3
bert3 <bert3@bert3.(none)>
2014-01-08 16:59:35 -0500

    new state estimator class to maintainvehicle state

---
+++ FRCProject/src/RobotCode/StateEstimator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package RobotCode;
+
+/**
+ *
+ * @author bert3
+ */
+public class StateEstimator {
+    
+    // enum for red and blue
+    boolean team_color_;  // red or blue
+    
+    double x_meters_;
+    double y_meters_;
+    double yaw_radians_;
+    
+    boolean ball_in_posession_;
+    
+    // more states
+    
+    
+}
