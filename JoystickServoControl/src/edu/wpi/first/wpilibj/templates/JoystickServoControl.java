ad267ca1d933a3aa849b81d2a28ddcc0b387b232
bert5 <bert5@bert5.(none)>
2014-01-17 05:08:51 -0500

    edited joy stick servo control for 4 servos and 2 joysticks

--- JoystickServoControl/src/edu/wpi/first/wpilibj/templates/JoystickServoControl.java
+++ JoystickServoControl/src/edu/wpi/first/wpilibj/templates/JoystickServoControl.java
@@ -22,11 +22,16 @@ import edu.wpi.first.wpilibj.Servo;
  * directory.
  */
 public class JoystickServoControl extends IterativeRobot {
-    Servo XtestServo = new Servo(10);
-    Servo YtestServo = new Servo(5);
+    Servo X1testServo = new Servo(10);
+    Servo Y1testServo = new Servo(5);
+    Servo X2testServo = new Servo(6);
+    Servo Y2testServo = new Servo(7);
     Joystick leftStick = new Joystick(1);
+    Joystick rightStick = new Joystick(2);
     
-    double joystickX, joystickY, mappedX, mappedY;
+    
+    double joystick1X, joystick1Y, mapped1X, mapped1Y, mapped2X, mapped2Y, joystick2X, joystick2Y;
+    
     
     /**
      * This function is run when the robot is first started up and should be
@@ -50,24 +55,46 @@ public class JoystickServoControl extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        joystick1X = leftStick.getAxis(Joystick.AxisType.kX);
+        joystick1Y = leftStick.getAxis(Joystick.AxisType.kY);
+        joystick2X = rightStick.getAxis(Joystick.AxisType.kX);
+        joystick2Y = rightStick.getAxis(Joystick.AxisType.kY);
         
+        mapped1Y = joystick1Y/2 + 0.5;
+        mapped1X = joystick1X/2 + 0.5;
+        mapped2X = joystick2X/2 + 0.5;
+        mapped2Y = joystick2Y/2 + 0.5;
+        
+        X1testServo.set(mapped1X);
+        Y1testServo.set(mapped1Y);
+        X2testServo.set(mapped2X);
+        Y2testServo.set(mapped2Y);
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {       
-        joystickX = leftStick.getAxis(Joystick.AxisType.kX);
-        joystickY = leftStick.getAxis(Joystick.AxisType.kY);
+        joystick1X = leftStick.getAxis(Joystick.AxisType.kX);
+        joystick1Y = leftStick.getAxis(Joystick.AxisType.kY);
+        joystick2X = rightStick.getAxis(Joystick.AxisType.kX);
+        joystick2Y = rightStick.getAxis(Joystick.AxisType.kY);
         
-        mappedY = joystickY/2 + 0.5;
-        mappedX = joystickX/2 + 0.5; 
-        XtestServo.set(mappedX);
-        YtestServo.set(mappedY);
-        System.out.println("X Servo Value = " + XtestServo.get());
-        System.out.println("Left Joystick X Value = " + leftStick.getX());
-        System.out.println("Y Servo Value = " + YtestServo.get());
-        System.out.println("Left Joystick Y Value = " + leftStick.getY());
+        mapped1Y = joystick1Y/2 + 0.5;
+        mapped1X = joystick1X/2 + 0.5;
+        mapped2X = joystick2X/2 + 0.5;
+        mapped2Y = joystick2Y/2 + 0.5;
+        
+        X1testServo.set(mapped1X);
+        Y1testServo.set(mapped1Y);
+        X2testServo.set(mapped2X);
+        Y2testServo.set(mapped2Y);
+        
+        
+        //System.out.println("X Servo Value = " + XtestServo.get());
+        //System.out.println("Left Joystick X Value = " + leftStick.getX());
+        //System.out.println("Y Servo Value = " + YtestServo.get());
+        //System.out.println("Left Joystick Y Value = " + leftStick.getY());
         
     
     }
