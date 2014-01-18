39fec21e394fd6e0a4ae1323c53f34b418e8fe2f
bert3 <bert3@bert3>
2014-01-18 15:27:09 -0500

    PneumaticsProjects

--- PracticeProjects/PneumaticsProject/src/RobotCode/PneumaticsProject.java
+++ PracticeProjects/PneumaticsProject/src/RobotCode/PneumaticsProject.java
@@ -10,7 +10,13 @@ package RobotCode;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
+
+
+
+
+
 /**
+ * 
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
