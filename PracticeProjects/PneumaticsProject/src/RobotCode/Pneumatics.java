55fda06a097e4c62d0231727c98a59b6c84b1dda
bert5 <bert5@bert5.(none)>
2014-02-04 16:50:43 -0500

    updates to the pneumatics project, getting rid of debug statements

--- PracticeProjects/PneumaticsProject/src/RobotCode/Pneumatics.java
+++ PracticeProjects/PneumaticsProject/src/RobotCode/Pneumatics.java
@@ -9,7 +9,7 @@ package RobotCode;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import java.util.Vector;
-
+//import edu.wpi.first.wpilibj.Relay;
 
 /**
  *
@@ -22,8 +22,14 @@ public class Pneumatics {
 
   Pneumatics(int pressure_switch_channel, int compressor_relay_channel) {
 
-    // pressure_switch_channel - digital input port to pressure switch that monitors the accumulator pressure
-    // compressor_relay_channel - digital output port to a digital (spike) relay that controls power to the compressor
+    // pressure_switch_channel - digital input port to pressure 
+    // switch that monitors the accumulator pressure, this gives
+    // us data about when the tank is full so we can turn off the
+    // compressor
+    // compressor_relay_channel - relay output port to a 
+    // digital (spike) relay that controls power to the 
+    // compressor (remember, this is relay output, not digital 
+    // output
     compressor_ = new Compressor(pressure_switch_channel, compressor_relay_channel);
     double_solenoids_list_ = new Vector();
     
@@ -31,11 +37,29 @@ public class Pneumatics {
     
   
   public void startCompressor() {
+    System.out.println("Starting Compressor...");
     compressor_.start();
   }
   
   public void stopCompressor() {
-    compressor_.stop();
+    if (compressor_.enabled()) {
+      System.out.println("Stopping Compressor...");
+      compressor_.stop();
+    }
+  }
+
+  public void enableLiveWindowMode() {
+    System.out.println("Enable live window mode...");
+    compressor_.startLiveWindowMode();
+  }
+  
+  public void disableLiveWindowMode() {
+    System.out.println("Disable live window mode...");
+    compressor_.stopLiveWindowMode();
+  }
+
+  public boolean pressureSwitchValue() {
+    return compressor_.getPressureSwitchValue();
   }
 
   // Push a new solenoid object onto our vector of solenoids.
--- PracticeProjects/PneumaticsProject/src/RobotCode/PneumaticsProject.java
+++ PracticeProjects/PneumaticsProject/src/RobotCode/PneumaticsProject.java
@@ -9,8 +9,8 @@ package RobotCode;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-
-
+//import edu.wpi.first.wpilibj.DigitalOutput;
+//import edu.wpi.first.wpilibj.Relay;
 
 
 
@@ -27,18 +27,25 @@ public class PneumaticsProject extends IterativeRobot {
 
     
     Pneumatics p;
-    
+  //DigitalOutput do_test_;
+  //Relay relay_;
     
     public void robotInit() {
 
 
-	int pressure_switch_channel = 5;
-	int compressor_relay_channel = 6;
-	p = new Pneumatics(pressure_switch_channel, compressor_relay_channel);
+        System.out.println("RobotInit");
+
+        //relay_ = new Relay(1);
         
-        int solenoid_1_forward = 7;
-        int solenoid_1_backward = 2;
-        p.addNewDoubleSolenoid(solenoid_1_forward, solenoid_1_backward);
+
+    //do_test_ = new DigitalOutput(1);
+        int pressure_switch_channel = 1; // should be a digital input
+	int compressor_relay_channel = 1; // should be a relay port
+	p = new Pneumatics(pressure_switch_channel, compressor_relay_channel);
+	
+        //int solenoid_1_forward = 7;
+        //int solenoid_1_backward = 5;
+        //p.addNewDoubleSolenoid(solenoid_1_forward, solenoid_1_backward);
     }
 
     /**
@@ -48,19 +55,31 @@ public class PneumaticsProject extends IterativeRobot {
 
     }
 
+    public void teleopInit() {
+        System.out.println("teleopInit");
+        //do_test_.set(true);
+        //p.startCompressor();
+	//boolean switchval = p.pressureSwitchValue();
+	//System.out.println("swith is " + switchval);
+	//p.enableLiveWindowMode();
+    }
+    
+    
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
 
         
-        p.startCompressor();
+            //p.startCompressor();
 
     }
     
-    public void diabledPeriodic() {
+    public void disabledPeriodic() {
 
-	p.stopCompressor();
+        //System.out.println("disabledperiodic!!!");
+        //do_test_.set(false);
+        p.stopCompressor();
 
     }
 
@@ -69,8 +88,21 @@ public class PneumaticsProject extends IterativeRobot {
     /**
      * This function is called periodically during test mode
      */
-    public void testPeriodic() {
+    public void testInit() {
+        System.out.println("testInit");
+  
+        
+        //relay_.set(Relay.Value.kForward);
+//do_test_.set(true);
+        p.startCompressor();
+	//int val = getDefaultDigitalModule();
+	//System.out.println("default digital module: " + val);
+
+	boolean switchval = p.pressureSwitchValue();
+	System.out.println("swith is " + switchval);
+    	//p.enableLiveWindowMode();
     
     }
+
     
 }
