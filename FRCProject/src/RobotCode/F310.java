811faa5900bfdb0cddbe6a6ee8d817f13e1efb2f
bert4 <bert4@bert4.(none)>
2014-02-07 18:04:23 -0500

    Fixed mapping for the f310

--- FRCProject/src/RobotCode/F310.java
+++ FRCProject/src/RobotCode/F310.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package RobotCode;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
@@ -12,82 +12,98 @@ import edu.wpi.first.wpilibj.Joystick;
  * @author user1
  */
 public class F310 {
+
     Joystick f130;
-    F310(int joystickPort){
+
+    F310(int joystickPort) {
         f130 = new Joystick(joystickPort);
-        
+
     }
-    public boolean getXButton(){
+
+    public boolean getXButton() {
+        return f130.getRawButton(3);
+    }
+
+    public boolean getAButton() {
         return f130.getRawButton(1);
+
     }
-    public boolean getAButton(){
+
+    public boolean getBButton() {
         return f130.getRawButton(2);
-        
+
     }
-    public boolean getBButton(){
-        return f130.getRawButton(3);
-        
-    }
-    public boolean getYButton(){
+
+    public boolean getYButton() {
         return f130.getRawButton(4);
-        
+
     }
-    public boolean getLBButton(){
-        return f130.getRawButton(5); //test
-        
+
+    public boolean getLBButton() {
+        return f130.getRawButton(5);
+
     }
-    public boolean getRBButton(){
-        return f130.getRawButton(6);  //test
-        
+
+    public boolean getRBButton() {
+        return f130.getRawButton(6);
+
     }
-    public boolean getLTButton(){ // Controls 0 to -1
-        return f130.getRawButton(3); 
-        
+
+    public double getLTButton() {
+        return f130.getRawAxis(3);
+
     }
-    public boolean getRTButton(){ // Controls 0 to 1
-        return f130.getRawButton(3);
-        
+
+    public double getRTButton() {
+        return f130.getRawAxis(3);
+
     }
-    public boolean getBackButton(){
-        return f130.getRawButton(9); //test
-        
+
+    public boolean getBackButton() {
+        return f130.getRawButton(7);
+
     }
-    public boolean getStartButton(){
+
+    public boolean getStartButton() {
+        return f130.getRawButton(8);
+
+    }
+
+    public boolean getL3Button() {
+        return f130.getRawButton(9);
+
+    }
+
+    public boolean getR3Button() {
         return f130.getRawButton(10);
-        
+
     }
-    public boolean getL3Button(){
-        return f130.getRawButton(11);
-        
-    }
-    public boolean getR3Button(){
-        return f130.getRawButton(12);
-        
-    }
-    public double getLeftStickX(){
-        return f130.getRawAxis(1);
-        
-    }
-    public double getLeftStickY(){
-        return f130.getRawAxis(2);
-        
-    }
-    public double getRightStickX(){
+
+    public double getLeftStickX() {
         return f130.getRawAxis(4);
-        
+
     }
-    public double getRightStickY(){   // Raw Axis Value may be incorrect
-        return f130.getRawAxis(0);
-        
+
+    public double getLeftStickY() {
+        return f130.getRawAxis(2);
+
     }
-    public double getDpadX(){
+
+    public double getRightStickX() {
+        return f130.getRawAxis(1);
+
+    }
+
+    public double getRightStickY() {
         return f130.getRawAxis(5);
-        
+
     }
-    public double getDpadY(){
+
+    public double getDpadX() {
         return f130.getRawAxis(6);
-        
+
     }
-    
-    
+   // public double getDpadY(){
+    //     return f130.getRawAxis();
+
 }
