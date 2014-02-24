8a71c0ba09611e80f0967cdd024d881b56e4f40c
bert2 <bert2@bert2.(none)>
2014-02-24 17:55:54 -0500

    add Autonous but not functional

---
+++ FRCProject/src/RobotCode/Autonomous.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package RobotCode;
+
+/**
+ *
+ * @author bert2
+ */
+
+public class Autonomous {
+
+double desired_shooting_distance = 24;
+    
+StateEstimator state_;
+MecDrive drive_;
+
+    Autonomous(StateEstimator state, MecDrive drive){
+        state_ = state;
+        drive_ = drive;
+        
+    }
+    
+    public void driveToShoot(){
+    
+    
+  }    
+    
+    
+    
+    
+    
+}
