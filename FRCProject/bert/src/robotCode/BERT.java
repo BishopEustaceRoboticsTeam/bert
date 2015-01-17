5c0ac0ff7739b8dcbf28efc7f1e0bae8720dd800
Reno <nfarnesi4@gmail.com>
2015-01-17 17:43:58 -0500

    Added vars section to BERT.java and overided all methods in iterative robot also added some comments. Added Drive.java class with comments to describe the code that needs to be written in it

--- FRCProject/bert/src/robotCode/BERT.java
+++ FRCProject/bert/src/robotCode/BERT.java
@@ -1,7 +1,9 @@
 
 package robotCode;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Talon;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -12,33 +14,100 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 
 
 public class BERT extends IterativeRobot {
+	//Controller vars:
+	final int F310_REMOTE_USB_PORT = 0;
+	F310 rc = new F310(F310_REMOTE_USB_PORT);
+	
+	//Pneumatics vars:
+	final int LIFTER_SOLENOID_PORT = 0;
+	final int LOCK_SOLENOID_PORT = 1;
+	Pneumatics pneu = new Pneumatics(LIFTER_SOLENOID_PORT, LOCK_SOLENOID_PORT);
+	
+	//Motor controller PWM ports:
+	final int FRONT_RIGHT_MOTOR = 0;
+	final int FRONT_LEFT_MOTOR = 1;
+	final int BACK_RIGHT_MOTOR = 2;
+	final int BACK_LEFT_MOTOR = 3;
+	
+	
+	//Digital sensor ports:
+	
+	
+	//Analog sensor ports:
+	
+	//Camera vars:
+	final int CAMERA_QUALITY = 50;
+	final String CAMERA_NAME = "cam0"; //the camera name (ex "cam0") can be found through the roborio web interface
+	CameraServer server;
+	
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-	F310 joy_1 = new F310(0);
-	
 	
     public void robotInit() {
+    	//set up the camera for viewing
+        server = CameraServer.getInstance();
+        server.setQuality(CAMERA_QUALITY);
+        server.startAutomaticCapture("CAMERA_NAME");
+        
    
+    }
+    
+    
+    //****AUTONOMOUS****:
+    
+    //this method is called once at the start of autonomous
+    
+    public void autonomousInit(){
+    	
+    	
     }
 
     /**
      * This function is called periodically during autonomous
      */
+    
     public void autonomousPeriodic() {
 
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
+  //****TELEOP****:
+    
+    
+   //this method is called once at the start of teleop
     public void teleopInit(){
     
     }
     
+    /**
+     * This function is called periodically during operator control
+     */
     public void teleopPeriodic() {
         
+    }
+    
+    //****DISABLED****:
+    
+    //this runs once at the start of disable
+    public void disabledInit(){
+    	
+    	
+    }
+    
+    //this runs periodically during disabled (loop)
+    public void disabledPeriodic(){
+    	
+    	
+    }
+    
+    //****TEST****:
+    
+    //this method is called once at the start of test
+    public void testInit(){
+    	
+    	
     }
     
     /**
---
+++ FRCProject/bert/src/robotCode/Drive.java
@@ -0,0 +1,33 @@
+package robotCode;
+//This class will handle moving the robot
+
+//imports
+
+
+
+public class Drive {
+	//global class vars go here
+	final int WHEEL_RADIUS = 3; //in inches
+	final double pi = 3.1459;
+	
+	
+	
+	public Drive(){
+		//constructor
+		
+	}
+ 
+	//controlleDrive method that needs to be called in teleop periodic
+	//it will drive the robot using the f310 remote
+	
+	//straight(double speed) method that will drive the robot straight using encoders
+	//it will take an input of a double between -1 - 1. 1: full forward  -1: full back
+	
+	//rightAngleTurn(var leftOrRight) //this will use the encoders to make
+	//a perfect right angle turn //need to decide what var type to use. enum? int? boolean
+	
+	//turn(int degree) //degree is 0-360 that will turn the robot using the encoders
+	//to the specified angle from the start position. counter clockwise! (90 is to the left)
+	
+
+}
