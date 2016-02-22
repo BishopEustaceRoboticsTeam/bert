c0f7b293ecf21448f3e5200d17a16c2423f098c1
bert 3 <figueroa.johnm@gmail.com>
2016-02-22 16:47:25 -0500

    More work on Vision Processing.

--- BertIV/src/org/usfirst/frc/team4750/robot/subsystems/VisionSystem.java
+++ BertIV/src/org/usfirst/frc/team4750/robot/subsystems/VisionSystem.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team4750.robot.subsystems;
 
+import java.util.Comparator;
+
 import org.usfirst.frc.team4750.robot.RobotValues;
+import org.usfirst.frc.team4750.robot.Robot.ParticleReport;
+import org.usfirst.frc.team4750.robot.Robot.Scores;
 
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.CannyOptions;
@@ -17,10 +21,57 @@ public class VisionSystem extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	int session;
 	
+	/* This is a structure to hold various measurements of a particle (ie, a connected section of 
+	 * the output of a binary mask). */
+	public class ParticleReport implements Comparator<ParticleReport>, Comparable<ParticleReport>{
+		double percentAreaToImageArea;
+		double area;
+		double boundingRectLeft;
+		double boundingRectTop;
+		double boundingRectRight;
+		double boundingRectBottom;
+		
+		@Override
+		public int compareTo(ParticleReport report) {
+			// TODO Auto-generated method stub
+			return (int) (report.area - this.area);
+		}
+
+		@Override
+		public int compare(ParticleReport report1, ParticleReport report2) {
+			// TODO Auto-generated method stub
+			return (int) (report1.area - report2.area);
+		}
+		
+	};
+	
+	//This is a structure to hold the scores of particles.
+	public class Scores{
+		public double AreaScore;
+		public double AspectScore;
+	};
+	
+	
+	//Images
 	Image inputFrame;
 	Image processedFrame;
+	
+	int imaqError;
+	
+	//Constants
+	NIVision.Range HUE_RANGE_1 = new NIVision.Range(0,63);	//Hue range for the tape 
+	NIVision.Range SAT_RANGE_1 = new NIVision.Range(3,89);	//Saturation range for the tape
+	NIVision.Range VAL_RANGE_1 = new NIVision.Range(90,213);	//Value (luminosity) range for the tape
+	double AREA_MINIMUM = 0.5; //Default Area minimum for particle as a percentage of total image area
+	double LONG_RATIO = 10/7; //A bounding retangle of the U-shaped target tape would have a length of 20 in. and a heigh of 14.
+	double SCORE_MIN = 75.0;  //Minimum score to be considered a tote
+	double VIEW_ANGLE = 52; //View angle fo camera, set to Axis m1011 by default, 64 for m1013, 51.7 for 206, 52 for HD3000 square, 60 for HD3000 640x480
+	NIVision.ParticleFilterCriteria2[] criteria = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1); //Don't know what these options do. They were the default in the example code, so until we learn more I'm sticking with them.
+	Scores scores = new Scores();
+
+	int session;
 	
 	public VisionSystem(){
 		//Create images
