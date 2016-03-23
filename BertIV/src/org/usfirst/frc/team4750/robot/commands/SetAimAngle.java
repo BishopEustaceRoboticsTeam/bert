f75df535937dcaa083028c40f72098fc8009090f
Reno <nfarnesi4@gmail.com>
2016-03-23 16:58:27 -0400

    Add IR sensor code for shooter arm motor

--- BertIV/src/org/usfirst/frc/team4750/robot/commands/SetAimAngle.java
+++ BertIV/src/org/usfirst/frc/team4750/robot/commands/SetAimAngle.java
@@ -2,47 +2,81 @@ package org.usfirst.frc.team4750.robot.commands;
 
 import org.usfirst.frc.team4750.robot.Robot;
 import org.usfirst.frc.team4750.robot.RobotValues;
+import org.usfirst.frc.team4750.robot.subsystems.Shooter;
+import org.usfirst.frc.team4750.robot.subsystems.Shooter.ShooterPos;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SetAimAngle extends Command{
 	
-	public SetAimAngle() {
+	ShooterPos targetPos;
+	ShooterPos currentPos;
+	
+	public SetAimAngle(ShooterPos targetPos) {
 		// TODO Auto-generated constructor stub
 		requires(Robot.shooter);
 		SmartDashboard.putBoolean("Has SetAimAngle.SetAimAngle() run?", true);
+		
+		this.targetPos = targetPos;
 	}
 
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
 		SmartDashboard.putBoolean("Has SetAimAngle.initialize() run?",true);
+
+		if (targetPos != currentPos) {
+			switch (targetPos) {
+				case HIGH_GOAL:
+					Robot.shooter.setShooterAimerMotorSpeed(0.5);
+					break;
+				case LOW_GOAL:
+					switch(currentPos){
+						case HIGH_GOAL:
+							Robot.shooter.setShooterAimerMotorSpeed(-0.5);
+							break;
+						case PICK_UP:
+							Robot.shooter.setShooterAimerMotorSpeed(0.5);
+							break;
+					}
+					break;
+				case PICK_UP:
+					Robot.shooter.setShooterAimerMotorSpeed(-0.5);
+					break;
+			}
+		}
+
 	}
 
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
 		SmartDashboard.putBoolean("Has SetAimAngle.execute run?", true);
-		Robot.shooter.setShooterAimerMotorSpeed(Robot.oi.getShooterY());
+		
+		
+		//Robot.shooter.setShooterAimerMotorSpeed(Robot.oi.getShooterY());
 	}
 
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
 		SmartDashboard.putBoolean("Has SetAimAngle.isFinished() run?", true);
-		return false;
+		return Robot.shooter.getIRSensor(targetPos);
 	}
 
 	@Override
 	protected void end() {
 		// TODO Auto-generated method stub
+		
 		SmartDashboard.putBoolean("Has SetAimAngle.end() run?", true);
 		
 		//SmartDashboard.putBoolean("Is SetAimAngle executing?", false);
 		//SmartDashboard.putBoolean("Is SetAimAngle.execute() running?", false);
 		//SmartDashboard.putBoolean("Is SetAimAngle.isFinished() running?", false);
 		
+		currentPos = targetPos;
+		Robot.shooter.setShooterAimerMotorSpeed(0);
 	}
 
 	@Override
